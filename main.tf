742ccb16780646690a7b232b7081649e4a4622dc
Abhivox <136894993+Abhivox@users.noreply.github.com>
2026-02-25 23:21:48 +0530

    Add Kubernetes deployment and service for megaproject

--- main.tf
+++ main.tf
@@ -6,4 +6,65 @@ resource "kubernetes_namespace" "mega_ns" {
     name = "megaproject"
   }
 }
+resource "kubernetes_deployment" "mega_project" {
+  metadata {
+    name      = "megaproject1"
+    namespace = kubernetes_namespace.mega_ns.metadata[0].name
+    labels = {
+      app = "megaproject1"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "megaproject1"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "megaproject1"
+        }
+      }
+
+      spec {
+        container {
+          image = "abhi13055/megaproject1"
+          name  = "megaproject1"
+
+          port {
+            container_port = 5000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "mega_service" {
+  metadata {
+    name      = "megaproject1"
+    namespace = kubernetes_namespace.mega_ns.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "megaproject1"
+    }
+
+    port {
+      port        = 80
+      target_port = 5000
+      node_port   = 30007
+    }
+
+    type = "NodePort"
+  }
+}
+
+
 
